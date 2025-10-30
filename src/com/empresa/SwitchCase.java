069edf1b22223ca309247692bfdad0912a5a2f99
Jimenez <sebastian.jimenez@softwareone.com>
2025-10-30 11:03:00 -0500

    fourth commit 30-10-2025 se adiciona la clase switchcase

---
+++ src/com/empresa/SwitchCase.java
@@ -0,0 +1,48 @@
+package com.empresa;
+
+public class SwitchCase 
+{
+
+	public static void main(String[] args) 
+	{
+		int dia = 9;
+		
+		switch(dia)
+		{
+		case 1:
+			System.out.println("Este es el día Lunes");
+			break;
+			
+		case 2:
+			System.out.println("Este es el día Martes");
+			break;
+			
+		case 3:
+			System.out.println("Este es el día Miércoles");
+			break;
+			
+		case 4:
+			System.out.println("Este es el día Jueves");
+			break;
+			
+		case 5:
+			System.out.println("Este es el día Viernes");
+			break;
+			
+		case 6:
+			System.out.println("Este es el día Sabado");
+			break;
+			
+		case 7:
+			System.out.println("Este es el día Domingo");
+			break;
+			
+			default:
+			System.out.println("Día no válido");
+						
+		}
+		
+
+	}
+
+}
