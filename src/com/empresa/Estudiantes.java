d8c3042cf36674c8396da7007d44bf6b84c20dd7
Jimenez <sebastian.jimenez@softwareone.com>
2025-10-30 15:33:26 -0500

    seventh commit 30-10-2025 se adiciona la clase estudiantes

---
+++ src/com/empresa/Estudiantes.java
@@ -0,0 +1,21 @@
+package com.empresa;
+
+public class Estudiantes 
+{
+	public Estudiantes()
+	{
+		System.out.println("Hola, estoy en el constructor");
+	}
+	
+	int numero;
+	String Nombre;
+	
+
+
+	public static void main(String[] args) 
+	{
+		Estudiantes est = new Estudiantes();
+		
+	}
+
+}
